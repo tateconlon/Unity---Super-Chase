3c62ab41b9102310edddcf3e37f95250e85b7065
Tate Conlon <tate.conlon@gmail.com>
2015-02-24 15:02:45 -0500

    Fixed CharacterControlled2D

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -3,171 +3,78 @@ using System.Collections;
 
 public class CharacterController2D : MonoBehaviour {
 
-	public float jumpFastAcceleration = 15f;
-	public float jumpSlowAcceleration = 10f;
-	public float wallJumpForce = 7.5f;
-	//public float walkMaxSpeed = 25f;
-	//public float runMaxSpeed = 35f;
-	public float slowAcceleration = 15f;
-	public float fastAcceleration = 20f;
-	public float wallCheckLength = 0.02f;
+	public float jumpFastAcceleration = 120f;
+	public float jumpSlowAcceleration = 120f;
+	public float wallFastJumpForce = 30f;
+	public float wallSlowJumpForce = 15f;
+	public float horzSlowAcceleration = 45f;
+	public float horzFastAcceleration = 75f;
+	public float wallCheckLength = 0.3f;
 	public float drawLength = 1f;
-	public float skinWidth = 0.1f;
-	//public float droppingFactor = 1.5f;
+	public float skinWidth = 0.2f;
 
-	public bool isJumping;
 	public bool isRunning;
-	public bool isDropping;
 
-	float currMaxSpeed;
-	float currAccel;
-
-	public float velX;
-	public float velY;
+	float currJumpAccel;
+	float currHorzAccel;
+	float currWallJumpForce;
 
 	Bounds box;
 
 	public bool grounded;
 	public bool onCeiling;
-	//public bool newOnCieling;
 	public bool onWallRight;
 	public bool onWallLeft;
-	public bool onAnyWall;
 
 	void Start(){
 		box = collider2D.bounds;
 	}
 
-	void Update() {
-		CheckWalls ();
-		if(Input.GetButton("Jump") || Input.GetAxisRaw("VerticalP2") > 0.01f)
-			isJumping = true;
-		else
-			isJumping = false;
-
-		if(Input.GetAxisRaw("VerticalP2") < -0.01f)
-			isDropping = true;
-		else
-			isDropping = false;
-
-		if(Input.GetKey (KeyCode.LeftShift))
-			isRunning = true;
-		else
-			isRunning = false;
-	}
-
 	void FixedUpdate() {
-
 		CheckWalls();
-		if(onAnyWall) {
-			velX = rigidbody2D.velocity.x;
-			velY = rigidbody2D.velocity.y;
-			//rigidbody2D.isKinematic = true;
 
-			if(isRunning) {
-				velX += fastAcceleration*Time.deltaTime * Input.GetAxisRaw("HorizontalP2");
-				velY += jumpFastAcceleration*Time.deltaTime * Input.GetAxisRaw("VerticalP2");
-			}
-			else {
-				velX += slowAcceleration*Time.deltaTime * Input.GetAxisRaw("HorizontalP2");
-				velY += jumpSlowAcceleration*Time.deltaTime * Input.GetAxisRaw("VerticalP2");
-			}
-
-			if(grounded)
-				velY = Mathf.Clamp (velY, 0, float.PositiveInfinity);
-			if(onCeiling)
-				velY = Mathf.Clamp (velY, float.NegativeInfinity, 0);
-			if(onWallLeft)
-				velX = Mathf.Clamp (velX, 0, float.PositiveInfinity);
-			if(onWallRight)
-				velX = Mathf.Clamp(velX, float.NegativeInfinity, 0);
-
-			if(isJumping) {
-				if(grounded)
-					velY = wallJumpForce;
-				else if(onCeiling)
-					velY -= wallJumpForce;
-				else  if(onWallLeft)
-					velX += wallJumpForce;
-				else if(onWallRight)
-					velX -= wallJumpForce;
-			}
-
-			rigidbody2D.velocity = new Vector2(velX, velY);
-		}
-		else{
-			rigidbody2D.isKinematic = false;
-			if(isRunning) {
-				rigidbody2D.AddForce(new Vector2(fastAcceleration*Time.deltaTime, 0f) * Input.GetAxisRaw("HorizontalP2"), ForceMode2D.Impulse);
-				rigidbody2D.AddForce(new Vector2(0f, jumpFastAcceleration*Time.deltaTime) * Input.GetAxisRaw("VerticalP2"), ForceMode2D.Impulse);
-			}
-			else {
-				rigidbody2D.AddForce(new Vector2(slowAcceleration*Time.deltaTime, 0f) * Input.GetAxisRaw("HorizontalP2"), ForceMode2D.Impulse);
-				rigidbody2D.AddForce(new Vector2(0f, jumpSlowAcceleration*Time.deltaTime) * Input.GetAxisRaw("VerticalP2"), ForceMode2D.Impulse);
-			}
-			if(onCeiling)
-				rigidbody2D.gravityScale = -1;
-			else
-				rigidbody2D.gravityScale = 1;
-		}
-	}
-
-	/*void FixedUpdate() {
-		if(isJumping) {
-			if(grounded || velY < 0f) {
-				velY = jumpSpeed*Time.deltaTime;
-				grounded = false;
-			}
-			else
-				velY += jumpSpeed*Time.deltaTime;
-			if(onWallLeft) {
-				velY = jumpSpeed;
-				velX = wallJumpSpeed;
-				onWallLeft = false;
-			}
-			else if(onWallRight){
-				velY = jumpSpeed;
-				velX = -wallJumpSpeed;
-				onWallRight = false;
-			}
-		}
-
-
-		if(isRunning) {
-			currMaxSpeed = runMaxSpeed;
-			currAccel = fastAcceleration;
+		//Set forces to fast if shift held
+		if(Input.GetKey(KeyCode.LeftShift)) {	//TODO: Better Input Managing, take out of Fixed Update?
+			currJumpAccel = jumpFastAcceleration;
+			currHorzAccel = horzFastAcceleration;
+			currWallJumpForce = wallFastJumpForce;
 		}
 		else {
-			currMaxSpeed = walkMaxSpeed;
-			currAccel = slowAcceleration;
+			currJumpAccel = jumpSlowAcceleration;
+			currHorzAccel = horzSlowAcceleration;
+			currWallJumpForce = wallSlowJumpForce;
 		}
-		velX += currAccel*Time.deltaTime*Input.GetAxisRaw("HorizontalP2");
-		velX = Mathf.Clamp(velX, -currMaxSpeed, currMaxSpeed);
 
-		if(onWallLeft && !grounded && velY > 0)
-			velX = 0;
-		else if(onWallLeft)
-			velX = Mathf.Clamp(velX, 0, currMaxSpeed);
-		if(onWallRight && !grounded)
-			velX = 0;
-		else if(onWallRight)
-			velX = Mathf.Clamp(velX, -currMaxSpeed, 0);
+		//Add forces according to Input
+		rigidbody2D.AddForce(new Vector2(currHorzAccel*Time.deltaTime, 0f) * Input.GetAxisRaw("HorizontalP2"), ForceMode2D.Impulse);
+		rigidbody2D.AddForce(new Vector2(0f, currJumpAccel*Time.deltaTime) * Input.GetAxisRaw("VerticalP2"), ForceMode2D.Impulse);
 
-		if(grounded)
-			velY = 0;
-		else if(newOnCieling && !onCeiling){	//First frame on cieling
-			velY = 0;
-			onCeiling = true;
-		}
-		else if(isDropping)
-			velY += Physics2D.gravity.y*Time.deltaTime*droppingFactor;
+		//If on wall and Jump is pressed, spring from wall
+		if(onCeiling && (Input.GetAxisRaw("VerticalP2") < 0 || Input.GetButton("Jump")))
+			rigidbody2D.AddForce(new Vector2(0f, -currWallJumpForce), ForceMode2D.Impulse);
+		if(grounded && (Input.GetAxisRaw("VerticalP2") > 0 || Input.GetButton("Jump")))
+			rigidbody2D.AddForce(new Vector2(0f, currWallJumpForce), ForceMode2D.Impulse);
+		if(onWallLeft && (Input.GetAxisRaw("HorizontalP2") > 0 || Input.GetButton("Jump")))
+			rigidbody2D.AddForce(new Vector2(currWallJumpForce, 0f), ForceMode2D.Impulse);
+		if(onWallRight && (Input.GetAxisRaw("HorizontalP2") < 0 || Input.GetButton("Jump")))
+			rigidbody2D.AddForce(new Vector2(-currWallJumpForce, 0), ForceMode2D.Impulse);
+
+		//Stick to ceiling
+		if(onCeiling)
+			rigidbody2D.gravityScale = -1;
 		else
-			velY += Physics2D.gravity.y*Time.deltaTime;
-
-		rigidbody2D.velocity = new Vector2(velX, velY);
-		newOnCieling = false;	//Reset every frame
+			rigidbody2D.gravityScale = 1;
 	}
-	*/
+
+	/*Checks walls from WallCheck Length away TOWARDS character's collider.  If the collider found is tagged as Platform, then they are on platform
+	Checks in 12 spots
+	_| | |_
+	_     _
+	_     _
+	 | | |
+	 Skin width brings the corner edges in so that when for example the charcter is on the ground, it does not trigger as onWallRight and onWallLeft as well
+	 (It lifts the bottom ones off the ground, top ones drop down etc.)
+	 */
 	void CheckWalls()
 	{
 		Vector3 rightInset = new Vector3(box.size.x/2f + wallCheckLength, 0f);
@@ -247,32 +154,6 @@ public class CharacterController2D : MonoBehaviour {
 		else {
 			onCeiling = false;
 		}
-
-		onAnyWall = grounded || onCeiling || onWallLeft || onWallRight;
 	}
-
-	void OnCollisionEnter2D(Collision2D coll)
-	{
-		if(coll.collider.tag == "Platform") {
-			rigidbody2D.isKinematic = true;
-			velX = rigidbody2D.velocity.x;
-			velY = rigidbody2D.velocity.y;
-			//onAnyWall = true;
-			CheckWalls();	//Should set onAnyWall to True
-			Debug.Log ("Wall Collision - OnAnyWall: " + onAnyWall);
-		}
-
-	}
-
-	void OnCollisionStay2D(Collision2D coll) {
-		if(coll.collider != null && coll.collider.tag == "Platform")
-			rigidbody2D.isKinematic = true;
-	}
-
-	void OnCollisionExit2D(Collision2D coll) {
-		if(coll.collider != null && coll.collider.tag == "Platform")
-			rigidbody2D.isKinematic = false;
-	}
-
 
 }
